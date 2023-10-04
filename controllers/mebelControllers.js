eeff491c1448082a80c5d906e7ba181609d0bed7
Shahzod95 <aktamovsh6975@gmail.com>
2023-10-05 03:13:34 +0500

    already project

--- controllers/mebelControllers.js
+++ controllers/mebelControllers.js
@@ -74,6 +74,8 @@ const updateMebel = async (req, res) => {
         const query = { _id: req.params.id };
         if(mebel) {
             let oldImgList = mebel.img_list
+            let oldImg = mebel.img
+            
             if(req.files){
                 req.files.img_list.map(i => {
                   oldImgList.push(i.path)   
@@ -87,34 +89,31 @@ const updateMebel = async (req, res) => {
                         }
                     })
                 }))
+                oldImg = req.files.img[0].path
+                fs.unlink(path.join(__dirname, "../", mebel.img), (err, data) => {
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("Remove image in mebel")
+                    }
+                })
             }
 
-            let oldImg = mebel.img
-            if(req.files){
-            oldImg = req.files.img[0].path
-            fs.unlink(path.join(__dirname, "../", mebel.img), (err, data) => {
-                if(err){
-                    console.log(err)
-                } else {
-                    console.log("Remove image in mebel")
-                }
-            })
-            }
 
-        let date = {
-            title: req.body.title,
-            old: req.body.old,
-            new: req.body.new,
-            mebel: req.body.mebel,
-            img_list: oldImgList,
-            img: oldImg,
-            size: req.body.size,
-            name: req.body.name,
-            subtitle: req.body.subtitle,
-            description: req.body.description
-        }
 
-        const mebel = await mebelModel.findOneAndUpdate(query, data, {
+        const mebel = await mebelModel.findOneAndUpdate(query, 
+            {
+                title: req.body.title,
+                old: req.body.old,
+                new: req.body.new,
+                mebel: req.body.mebel,
+                img_list: oldImgList,
+                img: oldImg,
+                size: req.body.size,
+                name: req.body.name,
+                subtitle: req.body.subtitle,
+                description: req.body.description
+            }, {
             new: true
         });
         res.status(201).json({
@@ -131,10 +130,50 @@ const updateMebel = async (req, res) => {
     }
 }
 
+const deleteMebel = async (req, res) => {
+    try {
+        const mebel = await mebelModel.findById(req.params.id)
+        if(mebel) {
+
+                mebel.img_list.map((item => {
+                    fs.unlink(path.join(__dirname, "../", item), (err, data) => {
+                        if(err){
+                            console.log(err)
+                        } else {
+                            console.log("Remove image list in mebel")
+                        }
+                    })
+                }))
+
+                fs.unlink(path.join(__dirname, "../", mebel.img), (err, data) => {
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("Remove image in mebel")
+                    }
+                })
+
+                res.status(201).json({
+                    message:"deleted mebel",
+                })
+            } else {
+                res.status(404).json({
+                    message:"Mebel Not Found"
+                })
+            }
+    } catch (error) {
+        res.status(400).json({
+            error:"Your request could not be processed. Please try again."
+        })
+    }
+}
+
+
 
   module.exports = {
     getMebels,
     getMebelById,
     addMebel,
-    updateMebel
+    updateMebel,
+    deleteMebel
   }
--- routes/mebelRoutes.js
+++ routes/mebelRoutes.js
@@ -4,7 +4,9 @@ const router = Router()
 const {
     getMebels,
     getMebelById,
-    addMebel
+    addMebel,
+    updateMebel,
+    deleteMebel
 } = require("../controllers/mebelControllers")
 
 const {upload} = require("../utils/multer")
@@ -12,5 +14,7 @@ const {upload} = require("../utils/multer")
 router.get('/', getMebels)
 router.get('/:id', getMebelById)
 router.post('/add', upload.fields([{name:"img_list", maxCount:20}, {name:"img"}]), addMebel)
+router.put('/:id', upload.fields([{name:"img_list", maxCount:20}, {name:"img"}]), updateMebel)
+router.delete("/:id", deleteMebel)
 
 module.exports = router
