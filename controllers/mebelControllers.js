8f9485fe525e60ef5cba12242271791b136b6227
Shahzod95 <aktamovsh6975@gmail.com>
2023-10-05 02:59:37 +0500

    added mebel updated controllers

--- controllers/mebelControllers.js
+++ controllers/mebelControllers.js
@@ -1,5 +1,8 @@
 const mebelModel = require("../models/mebel.Model");
 
+const fs = require("fs")
+const path = require("path")
+
 
 const getMebels = async (req, res) => {
     try {
@@ -65,9 +68,73 @@ const addMebel = async (req, res) => {
     }
 }
 
+const updateMebel = async (req, res) => {
+    try {
+        const mebel = await mebelModel.findById(req.params.id)
+        const query = { _id: req.params.id };
+        if(mebel) {
+            let oldImgList = mebel.img_list
+            if(req.files){
+                req.files.img_list.map(i => {
+                  oldImgList.push(i.path)   
+                })
+                mebel.img_list.map((item => {
+                    fs.unlink(path.join(__dirname, "../", item), (err, data) => {
+                        if(err){
+                            console.log(err)
+                        } else {
+                            console.log("Remove image list in mebel")
+                        }
+                    })
+                }))
+            }
+
+            let oldImg = mebel.img
+            if(req.files){
+            oldImg = req.files.img[0].path
+            fs.unlink(path.join(__dirname, "../", mebel.img), (err, data) => {
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("Remove image in mebel")
+                }
+            })
+            }
+
+        let date = {
+            title: req.body.title,
+            old: req.body.old,
+            new: req.body.new,
+            mebel: req.body.mebel,
+            img_list: oldImgList,
+            img: oldImg,
+            size: req.body.size,
+            name: req.body.name,
+            subtitle: req.body.subtitle,
+            description: req.body.description
+        }
+
+        const mebel = await mebelModel.findOneAndUpdate(query, data, {
+            new: true
+        });
+        res.status(201).json({
+            message:"Created",
+            mebel
+        })
+    } else {
+
+    }
+    } catch (error) {
+        res.status(400).json({
+            error:"Your request could not be processed. Please try again."
+        })
+    }
+}
+
 
   module.exports = {
     getMebels,
     getMebelById,
-    addMebel
+    addMebel,
+    updateMebel
   }
