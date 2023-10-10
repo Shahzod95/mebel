43caae2e6209773da26f4e99d10b8eee314732af
Shahzod95 <aktamovsh6975@gmail.com>
2023-10-11 00:09:36 +0500

    start4

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const setupDB = require('./utils/db');
 
 const app = express()
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //Body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -20,26 +20,26 @@ app.use('/api/mebel', require('./routes/mebelRoutes'))
 app.use('/api/user', require('./routes/userRoutes'))
 
 
-// const options = {
-//     definition:{
-//         openapi:"3.0.0",
-//         info:{
-//             title:"Mebel api doc",
-//             version: "0.1"
-//         },
-//         servers:[
-//             {
-//                 url:"https://mebel-j1jg.onrender.com/api/"
-//             },
-//             {
-//                 url:"http://localhost:5000/api/"
-//             },
-//         ]
-//     },
-//     apis: ["./routes/*.js"],
-// }
+const options = {
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title:"Mebel api doc",
+            version: "0.1"
+        },
+        servers:[
+            {
+                url:"https://mebel-j1jg.onrender.com/api/"
+            },
+            {
+                url:"http://localhost:5000/api/"
+            },
+        ]
+    },
+    apis: ["./routes/*.js"],
+}
 
-// const spacs = swaggerjsdoc(options)
-// app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
+const spacs = swaggerjsdoc(options)
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
 // const PORT = process.env.PORT || 5000
 app.listen()
