cc58f21edde76d112dd927433a5a5cdee6c558e8
Shahzod95 <aktamovsh6975@gmail.com>
2023-10-11 00:13:55 +0500

    dasddas

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
-const path = require('path')
-const swaggerjsdoc = require('swagger-jsdoc')
-const swaggerui = require('swagger-ui-express')
-const cors = require('cors')
-require('dotenv').config()
-const setupDB = require('./utils/db');
+// const express = require('express')
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose')
+// const path = require('path')
+// const swaggerjsdoc = require('swagger-jsdoc')
+// const swaggerui = require('swagger-ui-express')
+// const cors = require('cors')
+// require('dotenv').config()
+// const setupDB = require('./utils/db');
 
-const app = express()
+// const app = express()
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-//Body parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-setupDB()
+// //Body parser
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cors())
+// setupDB()
 
-app.use('/api/mebel', require('./routes/mebelRoutes'))
-app.use('/api/user', require('./routes/userRoutes'))
+// app.use('/api/mebel', require('./routes/mebelRoutes'))
+// app.use('/api/user', require('./routes/userRoutes'))
 
 
-const options = {
-    definition:{
-        openapi:"3.0.0",
-        info:{
-            title:"Mebel api doc",
-            version: "0.1"
-        },
-        servers:[
-            {
-                url:"https://mebel-j1jg.onrender.com/api/"
-            },
-            {
-                url:"http://localhost:5000/api/"
-            },
-        ]
-    },
-    apis: ["./routes/*.js"],
-}
+// const options = {
+//     definition:{
+//         openapi:"3.0.0",
+//         info:{
+//             title:"Mebel api doc",
+//             version: "0.1"
+//         },
+//         servers:[
+//             {
+//                 url:"https://mebel-j1jg.onrender.com/api/"
+//             },
+//             {
+//                 url:"http://localhost:5000/api/"
+//             },
+//         ]
+//     },
+//     apis: ["./routes/*.js"],
+// }
 
-const spacs = swaggerjsdoc(options)
-app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
-// const PORT = process.env.PORT || 5000
-app.listen()
+// const spacs = swaggerjsdoc(options)
+// app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
+// // const PORT = process.env.PORT || 5000
+// app.listen()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
