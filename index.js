15dedb4422ce6fcd68b33c3e00bacef115f2af9f
Shahzod95 <aktamovsh6975@gmail.com>
2023-10-11 17:07:41 +0500

    dassdwqeczxczqweerqwfas

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const http = require('http')
 const path = require('path')
 const swaggerjsdoc = require('swagger-jsdoc')
 const swaggerui = require('swagger-ui-express')
@@ -42,4 +41,10 @@ const options = {
 const spacs = swaggerjsdoc(options)
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
 // const PORT = process.env.PORT || 5000
-app.listen(5000)
+// app.listen(5000)
+
+const server = http.createServer(app)
+
+server.listen(5000, ()=> {
+    console.log("Server working ....")
+})
